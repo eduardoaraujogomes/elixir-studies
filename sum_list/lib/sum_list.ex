7cd94aeca8a15ca5254c6ddbc26c02afb8fe2d78
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-12-05 11:08:11 -0300

    Using Enum

--- sum_list/lib/sum_list.ex
+++ sum_list/lib/sum_list.ex
@@ -1,11 +1,15 @@
 defmodule SumList do
   def call(list), do: sum(list, 0)
 
-  def call_enum(list), do: Enum.map(list, fn elem -> elem + 1 end)
+  # def call_enum(list), do: Enum.map(list, fn elem -> elem + 1 end)
   # O Enum.sum  faz o papel de somar os valores em uma lista
   # Enum.max retorna o maior
   # Enum.min retorna o menor
   # Enum.map primeiro argumento é a lista o segundo é a função que vamos usar para fazer algo com nossos valores
+  # Enum.any? retorna um boolean
+  # Enum.map(%{a: 1, b: 2}, fn {_chave, valor} -> valor end)
+
+  def call_enum(list), do: Enum.any?(list, fn elem -> elem > 5 end)
 
   defp sum([], acc), do: acc
   # se a função for de uma linha pode usar o do: e tirar o end
