698d4dd42271c33f4bd1d204a0bfc995402172c0
Eduardo de Araújo Gomes Filho <73198111+eduardoaraujogomes@users.noreply.github.com>
2021-12-26 23:25:49 -0300

    Update sum_list.ex

--- sum_list/lib/sum_list.ex
+++ sum_list/lib/sum_list.ex
@@ -3,8 +3,8 @@ defmodule SumList do
 
   # def call_enum(list), do: Enum.map(list, fn elem -> elem + 1 end)
   # O Enum.sum  faz o papel de somar os valores em uma lista
-  # Enum.max retorna o maior
-  # Enum.min retorna o menor
+  # Enum.max retorna o maior.
+  # Enum.min retorna o menor.
   # Enum.map primeiro argumento é a lista o segundo é a função que vamos usar para fazer algo com nossos valores
   # Enum.any? retorna um boolean
   # Enum.map(%{a: 1, b: 2}, fn {_chave, valor} -> valor end)
